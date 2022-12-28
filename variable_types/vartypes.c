68fc8708febe4da21cf7e637c3356ff7808f4de3
Igor Melo <igoracmelo@protonmail.com>
2022-12-28 02:17:41 -0300

    variable types

---
+++ variable_types/vartypes.c
@@ -0,0 +1,22 @@
+#include <inttypes.h>
+#include <limits.h>
+
+int main ()
+{
+  // not portable!
+  {
+    int16_t i16;
+    uint32_t u32;
+  }
+
+  // portable :)
+  {
+    int_least16_t i16;
+    uint_least64_t u64;
+
+    // remember: wrapping int is undefined behavior
+    // remember: wrapping uint is not guaranteed to happen always in the same value
+  }
+
+  return 0;
+}
